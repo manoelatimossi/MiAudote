cc456185c43d9935c373e622fed6c5ceafc50486
manoelatimossi <manoelatimossi1@gmail.com>
2022-09-08 21:39:13 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:untitled/login.page.dart';
+import 'package:untitled/reset-password.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dog Life',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
